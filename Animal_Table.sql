9429a38f6cb82a2f10ac3f26233467f9ffc7cd4c
akren20 <ari_annek@ymail.com>
2024-02-04 20:08:31 -0600

    Add Animal_Table.sql

---
+++ Animal_Table.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE ZOODB;
+GO
+
+USE ZOODB;
+GO
+
+CREATE TABLE ANIMALS(
+ANIMAL_NAME VARCHAR(50) PRIMARY KEY,
+ANIMAL_AGE INT,
+ANIMAL_RES VARCHAR(50),	--where in the zoo the animals reside
+ANIMAL_FEEDFR INT,	--animals feeding frequency in hours
+ANIMAL_STAY INT	-- how long the animlas have stayed at the zoo
+);
+GO
+
+INSERT INTO ANIMALS (ANIMAL_NAME, ANIMAL_AGE, ANIMAL_RES, ANIMAL_FEEDFR, ANIMAL_STAY)
+VALUES ('Zebra',20, 'Outside',5,3);
+GO
+
+SELECT * FROM ANIMALS;
